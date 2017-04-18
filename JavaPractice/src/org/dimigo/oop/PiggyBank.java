4ca7c4c6d409f08b428d9d8a4f3d35e133280828
USER <USER@SEC>
2017-04-18 16:17:20 +0900

    실습과제8 <돼지 저금통>

---
+++ JavaPractice/src/org/dimigo/oop/PiggyBank.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *   |_PiggyBank
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 4. 18.
+ * </pre>
+ * 
+ * @author : USER
+ * @version : 1.8
+ */
+public class PiggyBank {
+	private static int balance;
+	
+	public static void putMoney(FamilyMember member, int amount){
+		System.out.println(member.getMemberName() + " : " + amount + "원 넣음");
+		balance += amount;
+	}
+	
+	public static void printBalance(){
+		System.out.println("돼지저금통 총 금액 : " + balance + "원");
+	}
+}
