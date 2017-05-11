3f61bc6a424ce58d7ccd4707c960a11871f1d9c3
USER <USER@SEC>
2017-05-11 15:28:30 +0900

    실습과제 10 <We Are the World!> (다형성 이용)

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package org.dimigo.inheritance;
+
+/**
+ * <pre>
+ * org.dimigo.inheritance
+ *   |_PersonTest
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 5. 8.
+ * </pre>
+ * 
+ * @author : USER
+ * @version : 1.8
+ */
+public class PersonTest2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		Person[] p = {
+				new Person("Tom"),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍")
+		};
+		
+		for (Person person : p){
+			greeting(person);
+		}
+		
+	}
+	private static void greeting(Person p){
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println();
+	}
+}
