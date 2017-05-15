d2451914dcf075926b0a1205cee0278d8ccfeff0
USER <USER@SEC>
2017-05-15 09:45:48 +0900

    실습과제11 <스마트폰 경쟁>

--- JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
+++ JavaPractice/src/org/dimigo/inheritance/SmartPhoneTest.java
@@ -20,7 +20,7 @@ public class SmartPhoneTest {
 	public static void main(String[] args) {
 		SmartPhone[] s = {
 				new IPhone("iPhone 7", "애플", 900000),
-				new Galaxy("갤럭시", "삼성", 800000)
+				new Galaxy("갤럭시 S8", "삼성", 800000)
 		};
 
 		for (SmartPhone phone : s){
